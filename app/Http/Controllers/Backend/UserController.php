54ffbc216f356451864cb74f59d4c59c38f72285
Dipendra <kdipendra642@gmail.com>
2022-04-28 01:25:05 +0545

    Manage User II

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -14,4 +14,67 @@ class UserController extends Controller
         $data['user'] = User::all();
         return view('backend.user.view_user', $data);
     }
+
+    public function UserAdd()
+    {
+        return view('backend.user.add_user');
+    }
+
+    public function UserStore(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|unique:users|email|max:255',
+            'password' => 'required'
+        ]);
+
+        $data = new User();
+        $data->usertype = $request->usertype;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->password = bcrypt($request->password);
+        $data->save();
+
+        $notification = array(
+            'message' => 'User inserted successfully.',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->route('user.view')->with($notification);
+    }
+
+    public function UserEdit($id)
+    {
+        $editData = User::find($id);
+        return view('backend.user.edit_user', compact('editData'));
+    }
+
+    public function UserUpdate(Request $request, $id)
+    {
+        $data = User::find($id);
+        $data->usertype = $request->usertype;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->save();
+
+        $notification = array(
+            'message' => 'User updated successfully.',
+            'alert-type' => 'info',
+        );
+
+        return redirect()->route('user.view')->with($notification);
+    }
+
+    public function UserDelete($id)
+    {
+        $notification = array(
+            'message' => 'User deleted successfully.',
+            'alert-type' => 'warning',
+        );
+
+        $data = User::find($id);
+        $data->delete();
+
+        return redirect()->route('user.view')->with($notification);
+    }
 }
--- resources/views/backend/user/view_user.blade.php
+++ resources/views/backend/user/view_user.blade.php
@@ -13,7 +13,7 @@
                     <div class="box">
                         <div class="box-header with-border">
                             <h3 class="box-title">User List</h3>
-                            <a href="#" style="float:right;" class="btn btn-rounded btn-success mb-5">Add User</a>
+                            <a href="{{ route('users.add')}}" style="float:right;" class="btn btn-rounded btn-success mb-5">Add User</a>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
@@ -36,8 +36,8 @@
                                             <td>{{ $user->name }}</td>
                                             <td>{{ $user->email }}</td>
                                             <td>
-                                                <a href="" class="btn btn-info">Edit</a>
-                                                <a href="" class="btn btn-danger">Delete</a>
+                                                <a href="{{ route('users.edit', $user->id) }}" class="btn btn-info">Edit</a>
+                                                <a href="{{ route('users.delete', $user->id) }}" class="btn btn-danger" id="delete">Delete</a>
                                             </td>
                                         </tr>
                                         @endforeach
