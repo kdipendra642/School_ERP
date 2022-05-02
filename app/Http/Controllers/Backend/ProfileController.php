7a3c47831c5739739d4a6fc08e48ef6f3b02b298
Dipendra <kdipendra642@gmail.com>
2022-05-03 02:03:47 +0545

    User Profile

---
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use App\Models\User;
+
+class ProfileController extends Controller
+{
+    public function ProfileView()
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        return view('backend.user.view_profile', compact('user'));
+    }
+
+    public function ProfileEdit()
+    {
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+
+        return view('backend.user.edit_profile', compact('editData'));
+    }
+
+    public function ProfileStore(Request $request)
+    {
+        $data = User::find(Auth::user()->id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->mobile = $request->mobile;
+        $data->address = $request->address;
+        $data->gender = $request->gender;
+
+        if ($request->file('image')) {
+            $file = $request->file('image');
+            @unlink(public_path('upload/user_images/', $data->image));
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'), $filename);
+            $data['image'] = $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'User profile updated.',
+            'alert-type' => 'info',
+        );
+
+        return redirect()->route('profile.view')->with($notification);
+    }
+
+    public function PasswordView(Request $request)
+    {
+        return view('backend.user.edit_password');
+    }
+}
---
+++ resources/views/backend/user/edit_password.blade.php
@@ -0,0 +1,75 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <div class="container-full">
+        <section class="content">
+
+            <!-- Basic Forms -->
+            <div class="box">
+                <div class="box-header with-border">
+                    <h4 class="box-title">Change Password</h4>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col">
+                            <form method="post" action="{{ route('profile.store') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <h5>Current Password <span class="text-danger">*</span></h5>
+                                    <div class="controls">
+                                        <input type="password" name="oldpassword" id="current_password" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <h5>New Password<span class="text-danger">*</span></h5>
+                                    <div class="controls">
+                                        <input type="password" name="password" id="password" class="form-control">
+                                        @error('password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <h5>Confirm Password<span class="text-danger">*</span></h5>
+                                    <div class="controls">
+                                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+                                        @error('password_confirmation')
+                                        <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="text-xs-right">
+                                    <input type="submit" class="btn btn-rounded btn-info mb-3" value="Update Password">
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+
+        </section>
+
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#image').change(function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#showImage').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    })
+</script>
+@endsection
